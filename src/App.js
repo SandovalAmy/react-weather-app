42f901036ef637608df8b7229f7f6823afc18a52
Amy Sandoval <amysandoval92@gmail.com>
2022-12-08 22:55:09 -0800

    Call forecast

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CurrentWeather from "./components/current-weather/CurrentWeather";
 import Search from "./components/search/Search";
 import { WEATHER_API_URL } from "./api";
 import { useState } from "react";
+import Forecast from "./components/forecast/Forecast";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
@@ -37,6 +38,7 @@ function App() {
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
